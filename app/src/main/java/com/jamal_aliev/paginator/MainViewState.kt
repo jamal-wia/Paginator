becd1a3b5ad2f5edbef9a53dec1d57c33e3b7510
Jamal Aliev <aliev.djamal@itms.tech>
2024-02-05 18:03:51 +0300

    "Sample" - added bg for page state and num page
    (cherry picked from commit f390dcc2a04a15931cf90f0a2cfcd15137cfede8)

--- app/src/main/java/com/jamal_aliev/paginator/MainViewState.kt
+++ app/src/main/java/com/jamal_aliev/paginator/MainViewState.kt
@@ -1,7 +1,10 @@
 package com.jamal_aliev.paginator
 
 sealed class MainViewState {
+
     data class DataState(
         val data: List<Paginator.PageState<String>>
     ) : MainViewState()
+
+    data object ProgressState : MainViewState()
 }
